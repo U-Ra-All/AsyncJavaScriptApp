8887b2a5cfaf46663e4ae9524b8c4edc70f18cad
YouRa <allakhverdovyuriy@gmail.com>
2021-12-02 13:33:33 +0200

    02-Первый AJAX вызов - XMLHttpRequest

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ////////////////////////////////////////////////////
+
+const getCoutnryData = function (countryName) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+  request.send();
+  // console.log(request.responseText);
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const currencies = data.currencies;
+    const currensyName = Object.values(currencies)[0].name;
+    const currensySymbol = Object.values(currencies)[0].symbol;
+
+    const languages = data.languages;
+    console.log(languages);
+    const firstLanguage = Object.values(languages)[0];
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👨‍👩‍👧‍👦</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} миллионов</p>
+      <p class="country__row"><span>🗣️</span>${firstLanguage}</p>
+      <p class="country__row"><span>💰</span>${currensySymbol} ${currensyName}</p>
+    </div>
+  </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCoutnryData('ukraine');
+getCoutnryData('canada');
+getCoutnryData('russia');
